ae141fa4dc5c6cae925fc07d3d438f6b06d7ec1b
Sergio Reinoso <slrs993@gmail.com>
2024-02-25 22:18:51 -0500

    AuthLayout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,33 +1,19 @@
 import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from "react-router-dom"
+import { AuthLayout } from "../layout/AuthLayout"
 
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={ 0 }
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding:  4 }}
-    >
-
-      <Grid item
-        className="box-shadow"
-        xs={ 3 }
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 1 }} >Login</Typography>
-
-        <form>
+    <AuthLayout title="Login">
+      <form>
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
               <TextField
                 label="Correo"
                 type="email"
-                placeholder="correo@google.com" 
+                placeholder="correo@google.com"
                 fullWidth
               />
             </Grid>
@@ -62,10 +48,7 @@ export const LoginPage = () => {
             </Grid>
 
           </Grid>
-        </form>
-
-      </Grid>
-
-    </Grid>
+      </form>
+    </AuthLayout>
   )
 }

2f1be40053e6d7aaa07861131117f9f7ea568543
Sergio Reinoso <slrs993@gmail.com>
2024-02-26 21:59:25 -0500

    Register page, JournalLayout and JournalPage with the fanally NavBar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material"
+import { NavBar } from "../components/NavBar"
+
+const drawerWidth = 240
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }} >
+
+        <NavBar drawerWidth={drawerWidth} />
+
+        {/* Sidebar drawerWidth */}
+
+        <Box
+            component='main'
+            sx={{ flexGrow: 1, p: 3 }}
+        >
+            { children }
+        </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+
+      <Typography>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis dolorum pariatur, quia molestias vero temporibus minus ad adipisci blanditiis. Magnam necessitatibus laudantium molestiae doloremque deserunt accusantium optio autem eos aliquid?
+      </Typography>
+
+    </JournalLayout>
   )
 }

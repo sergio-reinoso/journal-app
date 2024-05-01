fd68b8343f6177008f3fa46da67d13006a21b27f
Sergio Reinoso <slrs993@gmail.com>
2024-05-01 18:27:36 -0500

    JournalApp MUI Estructura y Diseño

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,33 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
+import { NoteView } from "../views/NoteView"
+import { IconButton } from "@mui/material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis dolorum pariatur, quia molestias vero temporibus minus ad adipisci blanditiis. Magnam necessitatibus laudantium molestiae doloremque deserunt accusantium optio autem eos aliquid?
-      </Typography>
+      </Typography> */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
 
     </JournalLayout>
   )
